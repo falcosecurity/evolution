229c6c80a506d9219a15551ce59ec650ed7ae030
Lorenzo Fontana <lo@linux.com>
2021-04-08 11:31:35 +0200

    update(usage-metrics/distribution-logs): remove visidata from decompress
    Co-Authored-By: Michele Zuccala <michele@zuccala.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- usage-metrics/distribution-logs/decompress.sh
+++ usage-metrics/distribution-logs/decompress.sh
@@ -4,8 +4,3 @@
 zcat _output/bucket/* > _output/all.log
 sed -i '/^#/ d' _output/all.log
 
-
-# alternative file for visidata processing
-
-zcat _output/bucket/*  | head -n 2 | tail -n 1 | sed 's/#Fields: //' | sed 's/ /\t/g' > _output/visidata.log
-cat _output/all.log >> _output/visidata.log
