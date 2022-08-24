d0883876b0ba39c764dd8caccb007fce3122ec3f
Leonardo Grasso <me@leonardograsso.com>
2022-08-24 17:07:16 +0200

    chore: clean up sandbox, moved to contrib repo
    See:
     -  https://github.com/falcosecurity/evolution/issues/170
     -  https://github.com/falcosecurity/contrib
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- usage-metrics/distribution-logs/decompress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# decompress and remove headers
-zcat _output/bucket/* > _output/all.log
-sed -i '/^#/ d' _output/all.log
-
--- usage-metrics/distribution-logs/fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm _output/bucket
-mkdir -p _output/bucket
-aws s3 cp  --recursive "s3://logging-falco-distribution/falco-distribution/" _output/bucket
-
-
