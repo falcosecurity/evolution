3bcb20bc9b60dce04f0d558e787cdee0c358a8fc
Jason Dellaluce <jasondellaluce@gmail.com>
2022-08-29 09:21:48 +0000

    new(utils): add tool for managing evolution repo
    Co-authored-by: Leonardo Grasso <me@leonardograsso.com>
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

---
+++ utils/pkg/utils/text.go
@@ -0,0 +1,74 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type TextEditFunc func(string) (string, error)
+
+func ReplaceTextTags(text, startTag, endTag, replace string) (string, error) {
+	start := 0
+	for {
+		start = strings.Index(text[start:], startTag)
+		if start < 0 {
+			return text, nil
+		}
+		start += len(startTag)
+		end := strings.Index(text[start:], endTag)
+		if end < 0 {
+			return "", fmt.Errorf("can't find end tag: " + endTag)
+		}
+		end += start
+		text = text[:start] + replace + text[end:]
+		start += len(replace) + len(endTag)
+	}
+}
+
+func EditTextFile(path string, editors ...TextEditFunc) error {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	edited := string(bytes)
+	for _, e := range editors {
+		edited, err = e(edited)
+		if err != nil {
+			return err
+		}
+	}
+	return ioutil.WriteFile(path, ([]byte)(edited), 0)
+}
+
+func EditCreateTextFile(path string, editors ...TextEditFunc) error {
+	err := EditTextFile(path, editors...)
+	if err != nil && os.IsNotExist(err) {
+		var f *os.File
+		f, err = os.Create(path)
+		if err == nil {
+			err = f.Close()
+			if err == nil {
+				err = EditTextFile(path, editors...)
+			}
+		}
+	}
+	return err
+}
---
+++ utils/pkg/utils/types.go
@@ -0,0 +1,82 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Maintainer struct {
+	Name     string   `yaml:"name"`
+	Github   string   `yaml:"github"`
+	Company  string   `yaml:"company"`
+	Projects []string `yaml:"projects"`
+}
+
+type Maintainers []Maintainer
+
+type RepositoryStatus string
+
+type Repository struct {
+	Name        string           `yaml:"name"`
+	Description string           `yaml:"description,omitempty"`
+	Status      RepositoryStatus `yaml:"status"`
+}
+
+type Repositories []Repository
+
+const (
+	RepositoryStatusOfficial   RepositoryStatus = "Official"
+	RepositoryStatusIncubating RepositoryStatus = "Incubating"
+	RepositoryStatusSandbox    RepositoryStatus = "Sandbox"
+	RepositoryStatusSpecial    RepositoryStatus = "Special"
+)
+
+func (r RepositoryStatus) String() string {
+	return string(r)
+}
+
+func (r *Repository) URL() string {
+	return fmt.Sprintf("https://github.com/falcosecurity/%s", r.Name)
+}
+
+func readFromFile(path string, out interface{}) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(data, out)
+}
+
+func ReadRepositoriesFromFile(path string) (Repositories, error) {
+	var res Repositories
+	if err := readFromFile(path, &res); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func ReadMaintainersFromFile(path string) (Maintainers, error) {
+	var res Maintainers
+	if err := readFromFile(path, &res); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
